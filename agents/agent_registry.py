072f8c46725ab0a0c33bdcd8f36720410692bdc7
tebriz91 <tebriz.tagiyev@gmail.com>
2024-10-13 16:53:46 +0300

    Refactor agent_registry.py to improve code organization and documentation

--- agents/agent_registry.py
+++ agents/agent_registry.py
@@ -1,18 +1,24 @@
 # Script for registering agents.
 
-from typing import TypedDict, Annotated, List, Any
-from langgraph.graph.message import add_messages
-
 # add_message creates this  [HumanMessage(content='Hello', id='1'), AIMessage(content='Hi there!', id='2')]
-from typing import TypedDict, Annotated, Any
-from langgraph.graph.message import add_messages
+from typing import Annotated, Any, List, TypedDict  # noqa
+
+from langgraph.graph.message import add_messages  # noqa
+
 
 # Define AgentRegistry as a TypedDict with total=False to allow extra keys
 class AgentRegistry(TypedDict, total=False):
     """
     A registry for agents, allowing dynamic addition of agents.
+
+    e.g.
+    Jar3d: Annotated[Any, add_messages]
+    RAGAgent: Annotated[Any, add_messages]
+    total=False allows us to add additional agents dynamically
     """
+
     user: List[Any]
+
 
 # Initialize the AgentRegistry as an empty dictionary
 AgentRegistry = {}
