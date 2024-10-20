8bbda09cee9b3618f8100e0160d7f7cfce52d271
tebriz91 <tebriz.tagiyev@gmail.com>
2024-10-20 23:22:51 +0300

    Comment out AgentRegistry TypeDict class in agent_registry.py

--- agents/agent_registry.py
+++ agents/agent_registry.py
@@ -1,15 +1,8 @@
-from typing import Annotated, Any, List, TypedDict  # noqa
-
-from langgraph.graph.message import add_messages  # noqa
-
+from typing import Any, List, TypedDict  # noqa
 
 # Define AgentRegistry as a TypedDict with total=False to allow extra keys
-class AgentRegistry(TypedDict, total=False):
-    # SerperDevAgent: Annotated[Any, add_messages]
-    # WebScraperAgent: Annotated[Any, add_messages]
-    # MetaAgent: Annotated[Any, add_messages]
-    # ReporterAgent: Annotated[Any, add_messages]
-    user: List[Any]
+# class AgentRegistry(TypedDict, total=False):
+#     user: List[Any]
 
 
 # Define a dictionary to store agent descriptions
